2c467e3ac86dfa701f803295109884239bd1fdfc
Shunyao Yang <>
2024-02-05 23:40:54 -0800

    separate the module/main

---
+++ module5_call.py
@@ -0,0 +1,11 @@
+from module5_mod import DataProcessor
+
+def main():
+    processor = DataProcessor()
+    processor.input_n()
+    processor.input_n_list()
+    processor.input_x()
+    processor.output_idx_of_inputs()
+
+if __name__ == "__main__":
+    main()
